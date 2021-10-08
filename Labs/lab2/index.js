33c4e1d093112112ba49c0184b6af41b740a732d
Xavier <xavier.dandigna@edu.ece.fr>
2021-10-08 09:04:17 +0200

    update

--- Labs/lab2/index.js
+++ Labs/lab2/index.js
@@ -2,21 +2,23 @@ const express = require('express')
 const metrics = require('./lib/metrics')
 const app = express()
 
+/*const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))*/
+
+
 app.set('port', 1337)
-
-
 
 app.set('views', __dirname + "/views")
 app.set('view engine', 'ejs');
 
 app.get(
-  '/hello/:name', 
-  (req, res) => res.send("Hello " + req.params.name)
+    '/hello/:name',
+    (req, res) => res.send("Hello " + req.params.name)
 )
 
 app.get('/metrics.json', async (req, res) => {
-  const data = await metrics.list()
-  res.status(200).json(data)
+    const data = await metrics.list()
+    res.status(200).json(data)
 })
 
 app.listen(

0562b1c210607903c039b9318b2d11bb9132253a
Xav <xavier.dandigna@edu.ece.fr>
2021-12-14 18:59:08 +0100

    feat : New purple footer

--- Project/front-end/src/Footer.js
+++ Project/front-end/src/Footer.js
@@ -1,18 +1,27 @@
 
 /** @jsxImportSource @emotion/react */
+//import './App.css';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
   footer: {
-    height: '30px',
-    backgroundColor: 'rgba(255,255,255,.3)',
-    flexShrink: 0,
+    backgroundColor: "#301934",
   },
-}
+  typography:{
+    margin: '6px',
+    textAlign: 'center',
+  }
+}));
 
-export default function Footer() {
+export default () => {
+  const styles = useStyles();
+  
   return (
-    <footer style={styles.footer}>
-      footer
+    <footer className={styles.footer}>
+      <Typography variant='subtitle2' className={styles.typography} component='div'>
+      <a href="https://github.com/Xavvert/WebTech_DANDIGNA_WALIGORA">Our Github</a>  -  Made by Paul Waligora & Xavier Dandigna  -  A SpaceChat' project
+      </Typography>
     </footer>
   );
 }
